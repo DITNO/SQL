2f4ab25c57cc4117db4245cda7d1aaed6a6122c8
DITNO <92884446+DITNO@users.noreply.github.com>
2023-07-26 23:05:20 +0530

    Update script1(commands).sql
    DQL and DML

--- script1(commands).sql
+++ script1(commands).sql
@@ -17,3 +17,21 @@ describe student_table;
 alter table student_table add email varchar(100);
 
 truncate table student_table;
+
+//DML
+
+insert into student_table (student_id,student_name,course_name,email)
+values (1,"prakash",'c1002','prakash@mail.com');
+
+insert into student_table (student_id,student_name,course_name,email)
+values (2,"akash",'c1003','akash@mail.com');
+
+insert into student_table (student_id,student_name,course_name,email)
+values (3,"bash",'c1004','bash@mail.com');
+
+describe student_table;
+
+//DQL
+select student_id,email,course_name from student_table;
+
+select * from student_table;
